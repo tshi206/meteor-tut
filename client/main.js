d8780c67e05579ab51efcf94976d51d151ade129
Mason <tshi206@aucklanduni.ac.nz>
2018-09-17 00:03:44 +1200

    done

--- client/main.js
+++ client/main.js
@@ -3,20 +3,84 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+const Resolutions = new Mongo.Collection('resolutions');
+
+// subscribe to the 'resolutions' publisher in server's main.js
+Meteor.subscribe("resolutions");
+
+// helpers defined in-memory variables for the selected template ( body-tag in this case )
+Template.body.helpers({
+    resolutions() {
+        console.log(Resolutions.find().fetch());
+        // session is no longer bundled in meteor default packages, install it with meteor cli via: meteor add session
+        if (Session.get('hideFinished')) {
+            return Resolutions.find({checked: {$ne: true}}); // retrieve entries with a non-true checked field value
+        }
+        return Resolutions.find(); // find all otherwise
+    },
+
+    hideFinished() {
+        return Session.get('hideFinished');
+    }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.body.events({
+    // register an onSubmit event on the form selected by class name 'new-resolution'
+    'submit .new-resolution': (event) => {
+        event.preventDefault(); // stop the submission from triggering a page reload
+        let title = event.target.title.value;
+
+        Meteor.call('addResolution', title);
+
+        event.target.title.value = "";
+    },
+
+    'change .hide-finished'(event) {
+        Session.set('hideFinished', event.target.checked);
+    }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.resolution.helpers({
+    isOwner() {
+        // this.owner is referring to the 'owner' fields inside the mongo collection
+        // noinspection JSUnresolvedVariable
+        return this.owner === Meteor.userId();
+    }
 });
+
+Template.resolution.events({
+    'click .toggle-checked'() {
+        Meteor.call("updateResolution", this._id, !this.checked);
+    },
+
+    'click .toggle-private'() {
+        Meteor.call('setPrivate', this._id, !this.isPrivate);
+    },
+
+    'click .delete'() {
+        // 'this' refers to resolution template, meteor magics...
+        Meteor.call('deleteResolution', this._id);
+    }
+});
+
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+});
+
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+//
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
--- server/main.js
+++ server/main.js
@@ -1,5 +1,53 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    // code to run on server at startup
+    // Mongo connection needs to be established on both the client and the sever side
+    const Resolutions = new Mongo.Collection('resolutions');
+
+    // remote services
+    Meteor.methods({
+        addResolution: title => {
+            Resolutions.insert({
+                title : title,
+                createdAt : new Date(),
+                owner: Meteor.userId()
+            });
+        },
+
+        updateResolution: (id, checked) => {
+            let resolution = Resolutions.findOne(id);
+            if (resolution.owner !== Meteor.userId()) {
+                throw new Meteor.Error('not-authorized');
+            }
+            Resolutions.update(id, {$set: {checked: checked}});
+        },
+
+        setPrivate: (id, isPrivate) => {
+            let resolution = Resolutions.findOne(id);
+            if (resolution.owner !== Meteor.userId()) {
+                throw new Meteor.Error('not-authorized');
+            }
+            Resolutions.update(id, {$set: {isPrivate: isPrivate}});
+        },
+
+        deleteResolution: id => {
+            let resolution = Resolutions.findOne(id);
+            if (resolution.owner !== Meteor.userId()) {
+                throw new Meteor.Error('not-authorized');
+            }
+            Resolutions.remove(id);
+        }
+    });
+
+    // publisher, restrict which parts of the collection we want to relay to the frontend subscriber. For example: if 'return Resolutions.find()', we simply relay every single entries inside the collection 'resolutions' to the subscriber.
+    Meteor.publish('resolutions', () => {
+        // return Resolutions.find()
+        return Resolutions.find({
+            $or: [
+                { isPrivate: {$ne: true} },
+                { owner: Meteor.userId() }
+            ]
+        })
+    });
 });
